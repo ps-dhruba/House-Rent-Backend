8b0753744bf70710843fdabbcd0e2759940eee78
Promit Sarker Dhruba <120182973+ps-dhruba@users.noreply.github.com>
2025-02-26 02:28:46 +0600

    Update urls.py

--- UserApp/urls.py
+++ UserApp/urls.py
@@ -5,9 +5,9 @@ router = DefaultRouter()
 
 router.register('User_list', views.UserViewset)
 urlpatterns = [
+    path('', include(router.urls)),
     path('register/', views.UserRegistrationApiView.as_view(), name='register'),
     path('login/', views.UserLoginApiView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
     path('active/<uid64>/<token>/', views.activate, name = 'activate'),
-    path('', include(router.urls)),
 ]
